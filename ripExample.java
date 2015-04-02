ca3aecdc2e5394d6c79ebe41b1ba49596ce14520
fattywompus <jace@jacerose.com>
2015-04-02 16:06:41 -0700

    Update ripExample.java
    is able to pull description now

--- ripExample.java
+++ ripExample.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 
 
@@ -11,21 +12,30 @@ public class ripExample
 
 	public static void main(String args[]) throws IOException
 	{
-		Document doc = Jsoup.connect("http://google.com/").get();
+		Document doc = Jsoup.connect("https://phet.colorado.edu/en/simulation/forces-and-motion-basics").get();
 
 		String html = "<p>An <a href='http://example.com/'><b>example</b></a> link.</p>";
 		//Document doc = Jsoup.parse(html);
-		Element link = doc.select("a").first();
+		Element element = doc.select("head").first();
+		
 
 		String text = doc.body().text(); // "An example link"
-		String linkHref = link.attr("span"); // "http://example.com/"
-		String linkText = link.text(); // "example""
-
-		String linkOuterH = link.outerHtml(); 
-		    // "<a href="http://example.com"><b>example</b></a>"
-		String linkInnerH = link.html(); // "<b>example</b>"
+		String linkHref = element.attr("title"); // "http://example.com/"
+		String linkText = element.text(); // "example""
 		
-		System.out.print(linkText);
+
+		System.out.println(linkText);
+		
+		
+		Elements elements = doc.select("meta");
+		element = elements.get(2);
+		
+		String str = element.attr("content");
+		//text = doc.body().text(); // "An example link"
+		
+		//linkText = element.text(); 
+		System.out.println(str);
+		
 	}
 	
 }
