10ac2d4e044115070ebae68800e4a3e08c80af27
BowenChan <BowenmChan@gmail.com>
2015-04-16 07:50:39 -0700

    Created an index file

--- data.php
+++ data.php
@@ -12,6 +12,7 @@ $password = "";
 $dbname = "educationhub";
 
 // Create connection
+$link = mysqli_connect($servername, $username,$password ,$dbname);
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
@@ -20,8 +21,8 @@ if ($conn->connect_error) {
 } 
 //echo "Connected successfully<br>\n";
 
-$sql = "SELECT id, title, description, lesson_link, lesson_image, category, student_grades, author, content_type, time_scraped FROM educationhub";
-$result = $conn->query($sql);
+$sql = "SELECT * FROM `education`";
+$result = mysqli_query($conn,$sql);
 ?>
 
 <table cellspacing = "30">
@@ -39,57 +40,54 @@ $result = $conn->query($sql);
     <td>Time Scraped</td>
   </tr>
 <?php
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-    	echo "<tr>";
-    	echo "<td>";
-    	echo $row["title"];
-    	echo "</td>";
+        while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+        {
+            echo "<tr>";
+            echo "<td>";
+            echo $row['title'];
+            echo "</td>";
 
-    	echo "<td>";
-    	echo $row["description"];
-    	echo "</td>";
+            echo "<td>";
+            echo $row["description"];
+            echo "</td>";
 
-    	echo "<td>"; ?>
-    	<a href ="<?php echo $row["lesson_link"]; ?>" >Link</a>
-    	<?php
-    	echo "</td>";
+            echo "<td>"; 
+            ?>
+               <a href ="<?php echo $row['lesson_link']; ?>" >Link</a>
+            <?php
+            echo "</td>";
 
-    	echo "<td>";?>
-    	<img src="<?php echo $row['lesson_image']; ?>" />
-    	<?php
-    	//echo $row["lesson_image"];
-    	//echo $data;
-    	echo "</td>";
+            echo "<td>";?>
+                <img src="<?php echo $row['lesson_image']; ?>" />
+            <?php
+            //echo $row["lesson_image"];
+            //echo $data;
+            echo "</td>";
 
-    	echo "<td>";
-    	echo $row["category"];
-    	echo "</td>";
+            echo "<td>";
+            echo $row["category"];
+            echo "</td>";
 
-    	echo "<td>";
-    	echo $row["student_grades"];
-    	echo "</td>";
+            echo "<td>";
+            echo $row["student_grades"];
+            echo "</td>";
 
-    	echo "<td>";
-    	echo $row["author"];
-    	echo "</td>";
+            echo "<td>";
+            echo $row["author"];
+            echo "</td>";
 
-    	echo "<td>";
-    	echo $row["content_type"];
-    	echo "</td>";
+            echo "<td>";
+            echo $row["content_type"];
+            echo "</td>";
 
-    	echo "<td>";
-    	echo $row["time_scraped"];
-    	echo "</td>";
-    	echo "</tr>";
+            echo "<td>";
+            echo $row["time_scraped"];
+            echo "</td>";
+            echo "</tr>";
+        }
 
 
-    }
-} else {
-    echo "0 results";
-}
-$conn->close();
+    $conn->close();
 ?>
 
 </table>
